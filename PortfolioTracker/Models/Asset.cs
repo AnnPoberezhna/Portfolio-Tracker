5219ba5a1f6adb19d4102256f5b7841f13738a87
jocker18291 <dawidans714@gmail.com>
2026-04-13 13:39:03 +0200

    Date validation added

--- PortfolioTracker/Models/Asset.cs
+++ PortfolioTracker/Models/Asset.cs
@@ -1,10 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace PortfolioTracker.Models
 {
-    public class Asset{
-
+    public class Asset : IValidatableObject
+    {
         // Primary key for the database
         [Key]
         public int Id {get; set; }
@@ -30,7 +31,22 @@ namespace PortfolioTracker.Models
         public decimal PurchasePrice { get; set; } 
 
         // Purchase date (defaults to the moment the entry is added)
+        [Required(ErrorMessage = "Please provide the purchase date.")]
+        [DataType(DataType.DateTime)]
         public DateTime PurchaseDate { get; set; } = DateTime.Now;
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var minDate = new DateTime(1900, 1, 1);
+            var maxDate = DateTime.Now;
+
+            if (PurchaseDate < minDate || PurchaseDate > maxDate)
+            {
+                yield return new ValidationResult(
+                    "Purchase date must be between 1900-01-01 and today.",
+                    new[] { nameof(PurchaseDate) });
+            }
+        }
 
     }
 }
